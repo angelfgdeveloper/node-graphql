58a8162e2c5df93bcdc6b618f8ca181db10c3ba8
Luis Angel <luis.flores@durango.gob.mx>
2024-06-25 14:01:02 -0600

    Listas y non-null en graphql

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,30 +1,37 @@
 const { ApolloServer } = require('apollo-server-express');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
+// El signo '!' => es para no tener nulos
 const typeDefs = `
   type Query {
-    hello: String
+    hello: String!
     getPerson(name: String, age: Int): String
-    getInt(age: Int): Int
+    getInt(age: Int!): Int
     getFloat(price: Float): Float
     getString: String
     getBoolean: Boolean
     getID: ID
+    getNumbers(numbers: [Int]!): [Int]
   }
 `;
 
 // GET => Query (Consultar datos)
 // POST, PUT, DELETE => Mutations
 
+// Lists
+// [String]
+// [Int]
+
 const resolvers = {
   Query: {
-    hello: () => 'Hola Mundo',
+    hello: () => null,
     getPerson: (_, args) => `Hello, my name is ${ args.name }, I'm ${ args.age } years old!`,
     getInt: (_, args) => args.age,
     getFloat: (_, args) => args.price,
     getString: () => "word",
     getBoolean: () => true,
     getID: () => '12345678',
+    getNumbers: (_, args) => args.numbers
   }
 }
 
