5d76a9c203b7f5fd6592a593d8c15fdfc791e6c3
Luis Angel <luis.flores@durango.gob.mx>
2024-07-16 12:43:12 -0600

    Conectando productosService

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -1,17 +1,23 @@
+const ProductsService = require('./../services/product.service');
+const service = new ProductsService();
 
-const getProduct = (_, { id }) => {
-  return { 
-    id, 
-    name: 'Product 1', 
-    price: 100.45, 
-    description: 'Lorem dsfsds sds d sdsd', 
-    image: 'http://image.asd', 
-    createdAt: new Date().toISOString(),
-  }
+const getProduct = async (_, { id }) => {
+  const product = await service.findOne(id);
+  return product;
+
+  // return { 
+  //   id, 
+  //   name: 'Product 1', 
+  //   price: 100.45, 
+  //   description: 'Lorem dsfsds sds d sdsd', 
+  //   image: 'http://image.asd', 
+  //   createdAt: new Date().toISOString(),
+  // }
 }
 
-const getProducts = () => {
-  return [];
+const getProducts = async () => {
+  const products = await service.find({});
+  return products;
 }
 
 const addProduct = () => {
